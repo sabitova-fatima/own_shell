50daaebec6a9125d62d17e24d736d8108c770851
Hearts Damion <sabitova.fatima@gmail.com>
2021-05-18 16:30:21 +0300

    second main added

---
+++ main_khouten.c
@@ -0,0 +1,16 @@
+#include "minishell.h"
+
+int main(int argc, char **argv, char **env)
+{
+	int i;
+	int j;
+	char *s;
+	char ****all;
+	int ****fd;
+
+	while (get_next_line(0, &s))
+	{
+		all = super_split(s, env, &fd);
+		free(s);
+	}
+}
