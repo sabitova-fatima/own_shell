d213d73cf5b520400f16f14c84e149d43e679fde
Hearts Damion <sabitova.fatima@gmail.com>
2021-07-13 11:32:24 +0300

    finish i hope...

--- main.c
+++ main.c
@@ -1,5 +1,53 @@
 #include "minishell.h"
 
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	negativ;
+
+	res = 0;
+	negativ = 1;
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			negativ = -1;
+		str++;
+		if (*str == '+' || *str == '-')
+			return (0);
+	}
+	if (*str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		res = 10 * res + *str - '0';
+		str++;
+	}
+	return (res * negativ);
+}
+
+char **env_with_shlvl(char **env)
+{
+	int 	shlvl;
+	char	*shlvl_char;
+	char 	*s;
+	char 	**env_copy;
+	int		pos;
+
+	env_copy = copy_env(env);
+	pos = where_to_put_env("SHLVL", env_copy);
+	shlvl_char = ft_substr(env_copy[pos], 6, 3);
+	shlvl = ft_atoi(shlvl_char);
+	free(shlvl_char);
+	shlvl_char = ft_itoa(shlvl + 1);
+	s = ft_strjoin("SHLVL=", shlvl_char);
+	free(env_copy[pos]);
+	env_copy[pos] = s;
+	free(shlvl_char);
+	return (env_copy);
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	***new;
@@ -9,8 +57,9 @@ int	main(int argc, char **argv, char **env)
 
 	(void)argc;
 	(void)argv;
-	env_copy = copy_env(env);
+
 	g_global.error_status = 0;
+	env_copy = env_with_shlvl(env);
 	while (1)
 	{
 		signal(SIGINT, ctrl_c);
